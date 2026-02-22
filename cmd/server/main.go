324cfbcd37885c16e54956d72663543518d4a5d0
iShawyha <shawyhaf@gmail.com>
2026-02-22 20:53:17 +0200

    chore: bootstrap Go API server with Postgres config

---
+++ cmd/server/main.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"log/slog"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	err := godotenv.Load()
+	if err != nil {
+		slog.Warn("No .env file found, relying on environment variables")
+	}
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	dsn := "postgres://" + dbUser + ":" + dbPass + "@" + dbHost + ":" + dbPort + "/" + dbName + "?sslmode=disable"
+	db, err := sqlx.Connect("postgres", dsn)
+	if err != nil {
+		slog.Error("Database connection failed", "error", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	r := gin.Default()
+	port := os.Getenv("PORT")
+	if port == "" {
+		slog.Error("PORT environment variable not set")
+		os.Exit(1)
+	}
+
+	slog.Info("Starting server", "port", port)
+	err = r.Run(":" + port)
+	if err != nil {
+		slog.Error("Server failed to start", "error", err)
+		os.Exit(1)
+	}
+}
